e3459a9682f0ec6c936fe3ec54f0176d8ce88b25
alvaroceu <alvaro.sanchezmateos@usp.ceu.es>
2023-04-20 22:29:30 +0200

    Clase Equipo + Atributos

---
+++ asanchezmBattleForFreedom/src/battleForFreedom/modelo/Equipo.java
@@ -0,0 +1,13 @@
+package battleForFreedom.modelo;
+
+/**
+ *
+ * @author Alvaro
+ */
+public class Equipo {
+
+    private Ejercito ejercito;
+    private int puntosDisponibles;
+    //Falta implementar: atributo "centroDeMando"
+
+}
