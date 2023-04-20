f90b24c53d56af9ff07b8d08dce6e773852ee499
alvaroceu <alvaro.sanchezmateos@usp.ceu.es>
2023-04-20 22:44:57 +0200

    Clase Unidad + Atributos

---
+++ asanchezmBattleForFreedom/src/battleForFreedom/modelo/Unidad.java
@@ -0,0 +1,17 @@
+package battleForFreedom.modelo;
+
+/**
+ *
+ * @author Alvaro
+ */
+public class Unidad {
+
+    private int costeUnidad;
+    private int energiaAtaque;
+    private int energiaDefensa;
+    private int energiaMovimiento;
+    private int potenciaAtaque;
+    private int puntosAnulado;
+    private Coordenada posicion;
+
+}
