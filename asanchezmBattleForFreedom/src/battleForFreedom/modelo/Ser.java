dead86ebdb5434dd165ebdafc628c91af6cd38b4
alvaroceu <alvaro.sanchezmateos@usp.ceu.es>
2023-04-20 22:51:30 +0200

    Clase Ser + Atributos

---
+++ asanchezmBattleForFreedom/src/battleForFreedom/modelo/Ser.java
@@ -0,0 +1,14 @@
+package battleForFreedom.modelo;
+
+/**
+ *
+ * @author Alvaro
+ */
+public class Ser {
+
+    private int costeSer;
+    private Raza raza;
+    private int resistenciaAtaques;
+    private int puntosAnulado;
+
+}
