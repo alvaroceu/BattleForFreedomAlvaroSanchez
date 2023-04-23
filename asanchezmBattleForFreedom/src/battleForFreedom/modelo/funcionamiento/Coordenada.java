bb3a0297d1ec660df72c3a6441f0a770e22c733a
alvaroceu <alvaro.sanchezmateos@usp.ceu.es>
2023-04-23 12:30:10 +0200

    Constructor de la Clase Coordenada

--- asanchezmBattleForFreedom/src/battleForFreedom/modelo/funcionamiento/Coordenada.java
+++ asanchezmBattleForFreedom/src/battleForFreedom/modelo/funcionamiento/Coordenada.java
@@ -1,5 +1,7 @@
 package battleForFreedom.modelo.funcionamiento;
 
+import java.util.Random;
+
 /**
  *
  * @author Alvaro
@@ -9,4 +11,13 @@ public class Coordenada {
     private int x;
     private int y;
 
+    /*
+    *  Constructor, genera una coordenada aleatoria
+    *  dentro del rango maximo que recibe como parametro
+     */
+    public Coordenada(int maximo) {
+        Random r = new Random();
+        this.x = r.nextInt(maximo);
+        this.y = r.nextInt(maximo);
+    }
 }
