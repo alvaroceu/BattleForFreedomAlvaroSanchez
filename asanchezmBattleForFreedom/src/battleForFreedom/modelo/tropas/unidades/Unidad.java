a5cb248ffb2b85b53bd84b8a946e53388ab7ac7e
alvaroceu <alvaro.sanchezmateos@usp.ceu.es>
2023-04-23 12:34:23 +0200

    Constructor de la Clase Unidad + Tipo Abstracto

--- asanchezmBattleForFreedom/src/battleForFreedom/modelo/tropas/unidades/Unidad.java
+++ asanchezmBattleForFreedom/src/battleForFreedom/modelo/tropas/unidades/Unidad.java
@@ -7,7 +7,7 @@ import battleForFreedom.modelo.tropas.seres.Ser;
  *
  * @author Alvaro
  */
-public class Unidad {
+public abstract class Unidad {
 
     private int costeUnidad;
     private int energiaAtaque;
@@ -17,5 +17,19 @@ public class Unidad {
     private int puntosAnulado;
     private Coordenada posicion;
     private Ser[] seresUnidad;
+
+    /*
+    *  Constructor completo, sera utilizado por las clases hijas
+     */
+    public Unidad(int costeUnidad, int energiaAtaque, int energiaDefensa, int energiaMovimiento, int potenciaAtaque, int puntosAnulado, Coordenada posicion, int numeroSeres) {
+        this.costeUnidad = costeUnidad;
+        this.energiaAtaque = energiaAtaque;
+        this.energiaDefensa = energiaDefensa;
+        this.energiaMovimiento = energiaMovimiento;
+        this.potenciaAtaque = potenciaAtaque;
+        this.puntosAnulado = puntosAnulado;
+        this.posicion = posicion;
+        this.seresUnidad = new Ser[numeroSeres];
+    }
 
 }
