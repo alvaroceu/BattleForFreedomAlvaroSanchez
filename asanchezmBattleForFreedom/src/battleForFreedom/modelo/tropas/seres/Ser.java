9e8bf4286981095d7f3ad2e9a81b7accd6eb232d
alvaroceu <alvaro.sanchezmateos@usp.ceu.es>
2023-04-23 11:53:09 +0200

    Constructor de la Clase Ser + Tipo abstracto

--- asanchezmBattleForFreedom/src/battleForFreedom/modelo/tropas/seres/Ser.java
+++ asanchezmBattleForFreedom/src/battleForFreedom/modelo/tropas/seres/Ser.java
@@ -6,11 +6,20 @@ import battleForFreedom.modelo.funcionamiento.Raza;
  *
  * @author Alvaro
  */
-public class Ser {
+public abstract class Ser {
 
     private int costeSer;
     private Raza raza;
     private int resistenciaAtaques;
     private int puntosAnulado;
 
+    /*
+    *  Constructor completo, se usara para las clases hijas de 'Ser'
+     */
+    public Ser(int coste, Raza raza, int resistencia, int puntos) {
+        this.costeSer = coste;
+        this.raza = raza;
+        this.resistenciaAtaques = resistencia;
+        this.puntosAnulado = puntos;
+    }
 }
