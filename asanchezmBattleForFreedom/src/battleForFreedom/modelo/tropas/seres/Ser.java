d9c2cdbaea28a40736ec3ae214b6c6cc5aefd4b1
alvaroceu <alvaro.sanchezmateos@usp.ceu.es>
2023-04-24 23:32:31 +0200

    JavaDoc de la clase Ser

--- asanchezmBattleForFreedom/src/battleForFreedom/modelo/tropas/seres/Ser.java
+++ asanchezmBattleForFreedom/src/battleForFreedom/modelo/tropas/seres/Ser.java
@@ -14,8 +14,14 @@ public abstract class Ser {
     private int puntosAnulado;
     private Boolean serAnulado;
 
-    /*
-    *  Constructor completo, se usara para las clases hijas de 'Ser'
+    /**
+     * Constructor de objetos de la clase Ser.
+     *
+     * @param coste Puntos necesarios para comprar el ser
+     * @param raza Humano o Na'vi
+     * @param resistencia Aguante frente a ataques
+     * @param puntos Puntos obtenidos al anular la unidad
+    *
      */
     public Ser(int coste, Raza raza, int resistencia, int puntos) {
         this.costeSer = coste;
@@ -25,18 +31,39 @@ public abstract class Ser {
         this.serAnulado = false;
     }
 
+    /**
+     * Permite modificar el valor de la resistencia frente a ataques.
+     *
+     * @param resistenciaAtaques Nuevo valor
+     */
     public void setResistenciaAtaques(float resistenciaAtaques) {
         this.resistenciaAtaques = resistenciaAtaques;
     }
 
+    /**
+     * Permite obtener el valor de la resistencia frente a ataques.
+     *
+     * @return valor de la resistencia frente a ataques.
+     */
     public float getResistenciaAtaques() {
         return resistenciaAtaques;
     }
 
+    /**
+     * Permite modificar el valor de serAnulado (True = ser anulado , False =
+     * ser NO anulado)
+     *
+     * @param serAnulado Nuevo valor (true, false)
+     */
     public void setSerAnulado(Boolean serAnulado) {
         this.serAnulado = serAnulado;
     }
 
+    /**
+     * Permite obtener la cantidad de puntos que se ganan al anular la unidad
+     *
+     * @returns Puntos ganados por anulacion.
+     */
     public int getPuntosAnulado() {
         return puntosAnulado;
     }
