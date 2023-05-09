5fd35a11f3e95358b06517635c552b3b6239f892
alvaroceu <alvaro.sanchezmateos@usp.ceu.es>
2023-05-09 12:16:04 +0200

    Añadida la condicion de victoria al método menuPrincipal

--- asanchezmBattleForFreedom/src/battleForFreedom/gui/Menu.java
+++ asanchezmBattleForFreedom/src/battleForFreedom/gui/Menu.java
@@ -11,30 +11,33 @@ public class Menu {
 
     public static void menuPrincipal(Partida partida) {
 
-        System.out.println("Turno de jugador: " + partida.getJugadorActual() + "\n"
-                + "¿Qué deseas hacer?\n\n"
-                + "1-Atacar\n"
-                + "2-Mover una unidad\n"
-                + "3-Comprar\n\n");
-
-        int opcion;
         do {
-            opcion = leerEntero("Escoje tu opcion: ");
-            if ((opcion < 1) || (opcion > 3)) {
-                System.out.println("La opcion escogida no existe, repite\n");
+            System.out.println("Turno de jugador: " + partida.getJugadorActual() + "\n"
+                    + "¿Qué deseas hacer?\n\n"
+                    + "1-Atacar\n"
+                    + "2-Mover una unidad\n"
+                    + "3-Comprar\n\n");
+
+            int opcion;
+            do {
+                opcion = leerEntero("Escoje tu opcion: ");
+                if ((opcion < 1) || (opcion > 3)) {
+                    System.out.println("La opcion escogida no existe, repite\n");
+                }
+            } while ((opcion < 1) || (opcion > 3));
+
+            switch (opcion) {
+                //Falta por implementar los métodos:
+                case 1:
+                    menuAtaque();
+                case 2:
+                    menuMover();
+                case 3:
+                    menuComprar();
             }
-        } while ((opcion < 1) || (opcion > 3));
 
-        switch (opcion) {
-            //Falta por implementar los métodos:
-            case 1:
-                menuAtaque();
-            case 2:
-                menuMover();
-            case 3:
-                menuComprar();
-        }
-
+            partida.pasarTurno();
+        } while (partida.getGanador() == null);
     }
 
 }
