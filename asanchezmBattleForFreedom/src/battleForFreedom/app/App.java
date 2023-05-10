91709b0ab85760df4aa175f91e09d2bb198a42bc
alvaroceu <alvaro.sanchezmateos@usp.ceu.es>
2023-05-10 15:40:46 +0200

    Creada la clase App con el main

---
+++ asanchezmBattleForFreedom/src/battleForFreedom/app/App.java
@@ -0,0 +1,17 @@
+package battleForFreedom.app;
+
+import static battleForFreedom.gui.Juego.comenzarJuego;
+
+/**
+ *
+ * @author Alvaro
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+        comenzarJuego();
+
+    }
+
+}
