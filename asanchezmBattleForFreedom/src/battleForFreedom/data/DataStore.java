865ab5047f708207cebfa2431a42c7c04107c220
alvaroceu <alvaro.sanchezmateos@usp.ceu.es>
2023-05-16 19:27:10 +0200

    Clase DataStore y preparada la clase Juego para poder almacenar partidas

---
+++ asanchezmBattleForFreedom/src/battleForFreedom/data/DataStore.java
@@ -0,0 +1,14 @@
+package battleForFreedom.data;
+
+import battleForFreedom.modelo.partida.Partida;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Alvaro
+ */
+public class DataStore {
+
+    private ArrayList<Partida> partidasGuardadas;
+
+}
--- asanchezmBattleForFreedom/src/battleForFreedom/gui/Juego.java
+++ asanchezmBattleForFreedom/src/battleForFreedom/gui/Juego.java
@@ -14,30 +14,48 @@ public class Juego {
 
     public static void comenzarJuego() {
 
-        System.out.println("¿En que escenario deseas jugar?\n\n"
-                + "1-Arbol de las Almas (100x100)\n"
-                + "2-Jardines Colgantes (200x200)\n"
-                + "3-Arbol Madre (300x300)\n");
-        int opcionEscenario;
+        int opcion;
         do {
-            opcionEscenario = leerEntero("Introduce tu opcion: ");
-            if ((opcionEscenario < 1) || (opcionEscenario > 3)) {
-                System.out.println("La opcion introducida no existe, repite.");
-            }
-        } while ((opcionEscenario < 1) || (opcionEscenario > 3));
+            System.out.println("¿Quieres buscar partidas guardadas?\n\n"
+                    + "1-Si\n"
+                    + "2-No, crear nueva\n");
+            opcion = leerEntero("Escoge tu opcion: ");
+        } while ((opcion < 1) || (opcion > 2));
 
-        Escenario escenario;
-        if (opcionEscenario == 1) {
-            escenario = new ArbolDeLasAlmas();
-        } else if (opcionEscenario == 2) {
-            escenario = new JardinesColgantes();
-        } else {
-            escenario = new ArbolMadre();
+        if (opcion == 2) {
+            System.out.println("¿En que escenario deseas jugar?\n\n"
+                    + "1-Arbol de las Almas (100x100)\n"
+                    + "2-Jardines Colgantes (200x200)\n"
+                    + "3-Arbol Madre (300x300)\n");
+            int opcionEscenario;
+            do {
+                opcionEscenario = leerEntero("Introduce tu opcion: ");
+                if ((opcionEscenario < 1) || (opcionEscenario > 3)) {
+                    System.out.println("La opcion introducida no existe, repite.");
+                }
+            } while ((opcionEscenario < 1) || (opcionEscenario > 3));
+
+            Escenario escenario;
+            if (opcionEscenario == 1) {
+                escenario = new ArbolDeLasAlmas();
+            } else if (opcionEscenario == 2) {
+                escenario = new JardinesColgantes();
+            } else {
+                escenario = new ArbolMadre();
+            }
+
+            Partida partida = new Partida(escenario, leerCadena("Introduce el nombre del jugador Humano: "), leerCadena("Introduce el nombre del jugador Navi: "));
+            menuPrincipal(partida);
+
+            if (partida.getGanador() != null) {
+                System.out.println("EL GANADOR ES: " + partida.getGanador());
+            }
         }
 
-        Partida partida = new Partida(escenario, leerCadena("Introduce el nombre del jugador Humano: "), leerCadena("Introduce el nombre del jugador Navi: "));
-        menuPrincipal(partida);
-        System.out.println("EL GANADOR ES: " + partida.getGanador());
+        if (opcion == 1) {
+
+        }
+
     }
 
 }
