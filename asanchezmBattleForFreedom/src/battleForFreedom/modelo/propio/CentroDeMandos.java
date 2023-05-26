19f129cd2fb637e545f1cd2bbe22622fb6ed18e0
alvaroceu <alvaro.sanchezmateos@usp.ceu.es>
2023-05-26 18:52:00 +0200

    Añadido JavaDoc de la clase CentroDeMandos

--- asanchezmBattleForFreedom/src/battleForFreedom/modelo/propio/CentroDeMandos.java
+++ asanchezmBattleForFreedom/src/battleForFreedom/modelo/propio/CentroDeMandos.java
@@ -12,15 +12,30 @@ public class CentroDeMandos {
     private ArrayList<String> equipoJugador;
     private ArrayList<String> mensajesPartida;
 
+    /**
+     * Constructor del centro de mandos que inicializa las listas.
+     */
     public CentroDeMandos() {
         this.equipoJugador = new ArrayList();
         this.mensajesPartida = new ArrayList();
     }
 
+    /**
+     * Método que permite añadir un mensaje a la lista de mensajes del juego
+     *
+     * @param mensaje mensaje para añadir
+     */
     public void actualizarMensajes(String mensaje) {
         this.mensajesPartida.add(mensaje + "\n");
     }
 
+    /**
+     * Método que, vacía la antigua lista de equipo, y la rellena con la
+     * información más actual, indicando los puntos del jugador en cuestión y
+     * todas sus unidades
+     *
+     * @param jugador jugador actual
+     */
     public void actualizarEquipo(Jugador jugador) {
         this.equipoJugador.clear();
         this.equipoJugador.add("Puntos disponibles: " + jugador.getPuntos());
@@ -30,10 +45,23 @@ public class CentroDeMandos {
         }
     }
 
+    /**
+     * Permite obtener la lista de mensajes de la partida de cada jugador para
+     * poder mostratla en los menús del juego.
+     *
+     * @return lista en cuestión
+     */
     public ArrayList<String> getMensajes() {
         return this.mensajesPartida;
     }
 
+    /**
+     * Permite obtener la lista de unidades y puntos disponibles de cada jugador
+     * para poder mostrarla en los menús del juego
+     *
+     * @param jugador jugador actual
+     * @return lista en cuestión
+     */
     public ArrayList<String> getEquipoJugador(Jugador jugador) {
         actualizarEquipo(jugador);
         return this.equipoJugador;
